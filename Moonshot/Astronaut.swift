e56784c1020dcd0f8e070f41dcbed6926d30b6be
Brian Foster <bfoster6@butlercc.edu>
2021-04-14 09:42:06 -0500

    Added images, JSON, and Astronaut decoder

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift'.swift
+//  Moonshot
+//
+//  Created by Professor Foster on 4/14/21.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -8,39 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var text = "Decode JSONss"
+    let astronauts = Bundle.main.decode("astronauts.json")
     
     var body: some View {
-        Button(text) {
-            let input = """
-            {
-                "name": "Taylor Swift",
-                "banana": "Yellow"
-                "address": {
-                    "street": "555, Taylor Swift Avenue",
-                    "city": "Nashville"
-                }
-            }
-            """
-            
-            struct User: Codable {
-                var name: String
-                var banana: String
-                var address: Address
-            }
-            
-            struct Address: Codable {
-                var street: String
-                var city: String
-            }
-            
-            let data = Data(input.utf8)
-            let decoder = JSONDecoder()
-            
-            if let user = try? decoder.decode(User.self, from: data) {
-                text = user.address.street
-            }
-        }
+        Text("\(astronauts.count)")
     }
 }
 
