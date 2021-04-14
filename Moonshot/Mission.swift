616ba313c435aede2673bb6aba34bf7715474dc7
Brian Foster <bfoster6@butlercc.edu>
2021-04-14 09:53:13 -0500

    Bundle extension to decode JSON

---
+++ Moonshot/Mission.swift
@@ -0,0 +1,22 @@
+//
+//  Mission.swift
+//  Moonshot
+//
+//  Created by Professor Foster on 4/14/21.
+//
+
+import Foundation
+
+
+
+struct Mission: Codable, Identifiable {
+    struct CrewRole: Codable {
+        let name: String
+        let role: String
+    }
+    
+    let id: Int
+    let launchDate: String?
+    let crew: [CrewRole]
+    let description: String
+}
