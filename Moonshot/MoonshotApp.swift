43fb02b13aa6c96f1873545b0dd716c8460096a3
Brian Foster <bfoster6@butlercc.edu>
2021-04-14 08:23:05 -0500

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Professor Foster on 4/14/21.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
