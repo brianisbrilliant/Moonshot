2d233d591bcd8c90f57e9707fd26a0ea623c2bca
Brian Foster <bfoster6@butlercc.edu>
2021-04-14 09:08:34 -0500

    Decoding JSON

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -8,14 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var text = "Decode JSONss"
+    
     var body: some View {
-        NavigationView {
-            List(0..<100) { row in
-                NavigationLink(destination: Text("Detail \(row)")) {
-                    Text("Row \(row)")
+        Button(text) {
+            let input = """
+            {
+                "name": "Taylor Swift",
+                "banana": "Yellow"
+                "address": {
+                    "street": "555, Taylor Swift Avenue",
+                    "city": "Nashville"
                 }
             }
-            .navigationBarTitle("SwiftUI")
+            """
+            
+            struct User: Codable {
+                var name: String
+                var banana: String
+                var address: Address
+            }
+            
+            struct Address: Codable {
+                var street: String
+                var city: String
+            }
+            
+            let data = Data(input.utf8)
+            let decoder = JSONDecoder()
+            
+            if let user = try? decoder.decode(User.self, from: data) {
+                text = user.address.street
+            }
         }
     }
 }
